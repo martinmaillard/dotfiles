84c22e1c81b25fb64bfec39683dc16ea7d22b621
Martin Maillard <git@martinmaillard.com>
2024-02-18 10:41:08 +0100

    Update zsh aliases

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -18,3 +18,20 @@ cleanpr() {
     git push origin :$1
     git branch -d $1
 }
+
+git-churn() {
+    git log --all -M -C --name-only --format='format:' "$@" \
+        | sort \
+        | grep -v '^$' \
+        | uniq -c \
+        | sort -n \
+        | awk 'BEGIN {print "count\tfile"} {print $1 "\t" $2}'
+}
+
+repo_url() {
+    cat "deps/$1/mix.exs" | grep "github.com" | grep $1 | sed -nE 's/^.*"(https?:\/\/w?w?w?\.?github.com\/.+\/.+)".*$/\1/p' | uniq
+}
+
+repo() {
+    repo_url $1 | xargs python -mwebbrowser -t
+}
